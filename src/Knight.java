6f51247019882b773897da6f11d828d057f42b73
Kyle Kim <kylekim@Kyles-MacBook-Air.local>
2023-02-18 20:40:06 -0800

    Movement restriction for Knight

--- src/Knight.java
+++ src/Knight.java
@@ -5,10 +5,16 @@ public class Knight extends Piece {
         super(black);
     }
 
-    @Override
     public ArrayList<BoardCoordinate> getPossibleMoves(BoardCoordinate position) {
         ArrayList<BoardCoordinate> possibleMoves = new ArrayList<>();
-        possibleMoves.add(new BoardCoordinate(position.x, position.y - 1)); // to up
+        possibleMoves.add(new BoardCoordinate(position.x - 2, position.y - 1));
+        possibleMoves.add(new BoardCoordinate(position.x - 1, position.y - 2));
+        possibleMoves.add(new BoardCoordinate(position.x + 1, position.y - 2));
+        possibleMoves.add(new BoardCoordinate(position.x + 2, position.y - 1));
+        possibleMoves.add(new BoardCoordinate(position.x + 2, position.y + 1));
+        possibleMoves.add(new BoardCoordinate(position.x + 1, position.y + 2));
+        possibleMoves.add(new BoardCoordinate(position.x - 1, position.y + 2));
+        possibleMoves.add(new BoardCoordinate(position.x - 2, position.y + 1));
         return possibleMoves;
     }
 }
